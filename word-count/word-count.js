812a2885e2af8b14eae0d8f52efc9e8d5f8b2b77
Alexander Glassford <a.b.glassford@gmail.com>
2016-05-29 18:23:04 -0600

    samesies

--- word-count/word-count.js
+++ word-count/word-count.js
@@ -1,8 +1,12 @@
 var Words = function(){
 };
 
-Words.prototype.count = function(word){
-return word + ": 1"
+Words.prototype.count = function(input){
+
+return 1
+
+
 };
 
 module.exports = Words
+//This is not an easy exercise, don't forget to check into regExp
