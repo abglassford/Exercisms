cb6a56824bc1303bfe4f14d1ee95ae86e4029d15
Alexander Glassford <a.b.glassford@gmail.com>
2016-06-07 16:05:14 -0600

    initial phone-number.js

--- phone-number/phone-number.js
+++ phone-number/phone-number.js
@@ -1,15 +1,27 @@
 var PhoneNumber = function(input){
-  this.ph = input
+  this.numbers = 1234567890
+  this.numArray1 = input.replace(')', '').replace('(', '').replace('-', '').replace(' ', '').replace(/\./g, '')
 }
 
 PhoneNumber.prototype.number = function(){
-  var numArray1 = this.ph.replace(/\)/g, '', /\(/g, '', /\-/,'', / /, '')
 
-  numArray2 = numArray1.replace(/\./g, '')
-
-return numArray1
+  if(this.numArray1.length > 10 && this.numArray1[0] === '1'){
+    this.numArray1 = this.numArray1.slice(1)
+  }else if(this.numArray1.length > 10 && this.numArray1[0] !== '1'){
+    return '0000000000'
+  }else if(this.numArray1.length < 10){
+    return '0000000000'
+  }
+return this.numArray1
 }
 
+PhoneNumber.prototype.areaCode = function(){
+  var code = this.numArray1.substring(0, 3)
+  return code
+}
 
+PhoneNumber.prototype.toString = function(){
+  
+}
 
 module.exports = PhoneNumber
