e97ecf525a2aef021d854c5bc2013f7ba94feaa4
Alexander Glassford <a.b.glassford@gmail.com>
2016-06-12 23:48:13 -0600

    add loops to program

--- anagram/anagram.js
+++ anagram/anagram.js
@@ -2,12 +2,17 @@ var Anagram = function(input){
   this.gram = input
 }
 
-Anagram.prototype.matches = function(){
-
-
-var empty = []
-
-return empty
+Anagram.prototype.matches = function(match){
+  var empty = []
+  for(var i = 0; i < this.gram.length; i++){
+    for(var j = 0; j < i; j++){
+      if(this.gram[i] === match[i].charAt(j)){
+        return match[i]
+      }else{
+        return empty
+      }
+  }
+  }
 }
 
 module.exports = Anagram
