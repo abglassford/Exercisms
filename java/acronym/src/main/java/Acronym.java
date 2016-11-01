da60be1a2dcc5b93c4b83fc279b99831b875d647
Alexander Glassford <a.b.glassford@gmail.com>
2016-11-01 11:41:35 -0600

    work on Acronym.java

---
+++ java/acronym/src/main/java/Acronym.java
@@ -0,0 +1,15 @@
+public class Acronym {
+  public static String generate (String phrase){
+    String result = "";
+    String[] resultsArr = phrase.split(" ");
+    for (int i = 0; i < resultsArr.length; i++){
+      result += Character.toUpperCase(resultsArr[i].charAt(0));
+      for (int j = 1; j < resultsArr[i].length(); j++) {
+        if (Character.isUpperCase(resultsArr[i].charAt(j))) {
+          result += resultsArr[i].charAt(j);
+        }
+      }
+    }
+    return result;
+  }
+}
---
+++ php/gigasecond/gigasecond.php
@@ -0,0 +1,6 @@
+<?php
+
+class GigasecondTest {
+  public function
+
+}
