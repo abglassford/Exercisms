989c17dff3e058072e0e040b6cc8e331b1f830a5
Alexander Glassford <a.b.glassford@gmail.com>
2016-11-03 20:55:38 -0600

    fix up code

--- php/hamming/hamming.php
+++ php/hamming/hamming.php
@@ -1,22 +1,15 @@
 <?php
-
-//
-// This is only a SKELETON file for the "Hamming" exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
-function distance($a, $b)
-{
+function distance($a, $b) {
   if (strlen($a) != strlen($b)) {
     throw new InvalidArgumentException('DNA strands must be of equal length.');
   }
   $count = 0;
-  $arr1 = str_split($a);
-  $arr2 = str_split($b);
-  for ($i=0; $i <= count($arr1); $i++) {
-    if ($arr1{$i} != $arr2{$i}) {
+  for ($i = 0; $i < strlen($a); $i++) {
+    if ($a[$i] != $b[$i]) {
       $count++;
     };
   };
   return $count;
 }
+
+echo distance('GGACG', 'GGTCG');
