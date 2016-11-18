7313e98f32953604a39df37655da4e6752209431
Alexander Glassford <a.b.glassford@gmail.com>
2016-11-17 22:05:45 -0700

    complete space-age.js

--- javascript/binary-search/binary-search.js
+++ javascript/binary-search/binary-search.js
@@ -1,17 +1,34 @@
-function BinarySearch (array) {
-  var count = 0;
-  array.forEach((el, i) => {
-    if (array.sort()[i] === el) {
-      count++
-    }
-  })
-  if (count === array.length) {
-    this.array = array;
+class BinarySearch {
+  constructor (array) {
+    const isArray = array.every((el, i, arr) => {
+      if (arr[i - 1] === undefined) {
+        return el
+      } else {
+        return el >= arr[i - 1]
+      }
+    })
+    if (isArray) this.array = array;
   }
 
+  indexOf (element) {
+    let i = Math.floor((this.array.length - 1) / 2);
+    if (this.array[i] === element) {
+      return i;
+    } else {
+      if (this.array[i] < element) {
+        this.array = this.array.slice(0, i)
+        this.indexOf(element)
+      }
+      if (this.array[i] > element) {
+        this.array = this.array.slice(i, this.array.length - 1)
+        this.indexOf(element)
+      }
+    }
+  }
+
+
+
+
 }
-
-
-
 
 module.exports = BinarySearch;
